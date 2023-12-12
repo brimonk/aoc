8155a218bb3de14512e1cf93e25f9b58beeece46
Brian Chrzanowski <chrzanowskitest@gmail.com>
2023-12-12 11:34:20 -0500

    [2023] solved day 7

--- 2023/d7.c
+++ 2023/d7.c
@@ -1,19 +1,19 @@
 #include "common.h"
 
 enum {
-    HANDKIND_HI,
-    HANDKIND_ONE,
-    HANDKIND_TWO,
-    HANDKIND_THREE,
-    HANDKIND_FULL,
-    HANDKIND_FOUR,
-    HANDKIND_FIVE,
+    HANDKIND_HI = 0,
+    HANDKIND_ONE = 1,
+    HANDKIND_TWO = 2,
+    HANDKIND_THREE = 3,
+    HANDKIND_FULL = 4,
+    HANDKIND_FOUR = 5,
+    HANDKIND_FIVE = 6,
     HANDKIND_TOTAL
 };
 
 struct hand {
-    char cards[5];
-    int hand_vals[5];
+    char cards[6];
+    char valus[6];
     int kind;
     int bet;
 };
@@ -35,46 +35,50 @@ int card_2_hex(char c)
     }
 }
 
+int card_value(char c)
+{
+    switch (c) {
+        case 'A': return 14;
+        case 'K': return 13;
+        case 'Q': return 12;
+        case 'J': return 11;
+        case 'T': return 10;
+        default: return  c - '0';
+    }
+}
+
 int hand_kind(struct hand *h)
 {
     int slots[15] = {0};
     for (int i = 0; i < 5; i++) {
-        slots[h->hand_vals[i]]++;
+        slots[card_value(h->cards[i])]++;
     }
     int combos[HANDKIND_TOTAL] = {0};
     for (int i = 0; i < 15; i++) {
+        if (slots[i] == 0)
+            continue;
         switch (slots[i]) {
             case 0: combos[HANDKIND_HI]++; break;
-            case 1: combos[HANDKIND_ONE]++; break;
             case 2: combos[HANDKIND_TWO]++; break;
             case 3: combos[HANDKIND_THREE]++; break;
             case 4: combos[HANDKIND_FOUR]++; break;
             case 5: combos[HANDKIND_FIVE]++; break;
+            default: break;
         }
     }
-    if (combos[HANDKIND_FIVE]) return HANDKIND_FIVE;
-    if (combos[HANDKIND_FOUR]) return HANDKIND_FOUR;
-    if (combos[HANDKIND_THREE] && combos[HANDKIND_TWO]) return HANDKIND_FULL;
-    if (combos[HANDKIND_THREE]) return HANDKIND_THREE;
-    if (combos[HANDKIND_TWO]) return HANDKIND_TWO;
-    if (combos[HANDKIND_ONE]) return HANDKIND_ONE;
+    if (combos[HANDKIND_FIVE])
+        return HANDKIND_FIVE;
+    if (combos[HANDKIND_FOUR])
+        return HANDKIND_FOUR;
+    if (combos[HANDKIND_THREE] && combos[HANDKIND_TWO])
+        return HANDKIND_FULL;
+    if (combos[HANDKIND_THREE])
+        return HANDKIND_THREE;
+    if (combos[HANDKIND_TWO] == 2)
+        return HANDKIND_TWO;
+    if (combos[HANDKIND_TWO])
+        return HANDKIND_ONE;
     return HANDKIND_HI;
-}
-
-int int_cmp(const void *a, const void *b)
-{
-    int ia = *(const int *)a;
-    int ib = *(const int *)b;
-    return ib - ia;
-}
-
-int card_cmp(const void *a, const void *b)
-{
-    char ia = *(const int *)a;
-    char ib = *(const int *)b;
-    int va = card_2_hex(ia);
-    int vb = card_2_hex(ib);
-    return va - vb;
 }
 
 struct hand parse_hand(char **s)
@@ -85,11 +89,13 @@ struct hand parse_hand(char **s)
 
     for (int i = 0; i < 5; i++)
         h.cards[i] = s[0][i];
-    qsort(h.cards, 5, sizeof(char), card_cmp);
+
     for (int i = 0; i < 5; i++)
-        h.hand_vals[i] = card_2_hex(s[0][i]);
+        h.valus[i] = card_2_hex(h.cards[i]);
+
     h.bet = atoi(s[1]);
     h.kind = hand_kind(&h);
+
     return h;
 }
 
@@ -97,23 +103,14 @@ int hand_cmp(const void *a, const void *b)
 {
     struct hand *ha = (struct hand *)a;
     struct hand *hb = (struct hand *)b;
-
-    // sort by hand kind first
-    if (ha->kind != hb->kind) {
-        return hb->kind - ha->kind;
-    }
-
-    for (int i = 0; i < 5; i++) {
-        if (ha->hand_vals[i] != hb->hand_vals[i]) {
-            return hb->hand_vals[i] - ha->hand_vals[i];
-        }
-    }
-    return 0;
+    if (hb->kind == ha->kind)
+        return strcmp(hb->valus, ha->valus);
+    return hb->kind - ha->kind;
 }
 
 void p1()
 {
-    int ans = 0;
+    i64 ans = 0;
     char buf[1024];
     struct hand *hands = NULL;
 
@@ -132,20 +129,145 @@ void p1()
     qsort(hands, arrlen(hands), sizeof(hands[0]), hand_cmp);
 
     for (int i = 0; i < arrlen(hands); i++) {
-        fprintf(stderr, "\t%.*s - %d - %d\n", 5, hands[i].cards, hands[i].kind, hands[i].bet);
         ans += hands[i].bet * (arrlen(hands) - i);
+#if 0
+        fprintf(stderr, "\t%.*s - %.*s - %d - %d - %ld\n",
+            5, hands[i].cards, 5, hands[i].valus, hands[i].kind, hands[i].bet, ans
+        );
+#endif
     }
 
-    printf("p1: %d\n", ans);
+    printf("p1: %ld\n", ans);
 
     arrfree(hands);
 }
 
+int card_2_hex_2(char c)
+{
+    switch (c) {
+        case 'A': return HEX[14];
+        case 'K': return HEX[13];
+        case 'Q': return HEX[12];
+        case 'J': return '1';
+        case 'T': return HEX[10];
+        default: return HEX[c - '0'];
+    }
+}
+
+int card_value_2(char c)
+{
+    switch (c) {
+        case 'A': return 14;
+        case 'K': return 13;
+        case 'Q': return 12;
+        case 'J': return 1;
+        case 'T': return 10;
+        default: return  c - '0';
+    }
+}
+
+int hand_kind_2(struct hand *h)
+{
+    int slots[15] = {0};
+    for (int i = 0; i < 5; i++) {
+        slots[card_value_2(h->cards[i])]++;
+    }
+
+    // translate "jokers" into the highest of whatever other card there is
+    if (slots[1] >= 1) {
+        // find the highest
+        int c = 0;
+        int m = 0;
+        for (int i = 2; i < 15; i++) {
+            if (c <= slots[i] && m < i) {
+                c = slots[i];
+                m = i;
+            }
+        }
+
+        int t = slots[1];
+        slots[1] -= t;
+        slots[m] += t;
+    }
+
+    int combos[HANDKIND_TOTAL] = {0};
+    for (int i = 0; i < 15; i++) {
+        if (slots[i] == 0)
+            continue;
+        switch (slots[i]) {
+            case 0: combos[HANDKIND_HI]++; break;
+            case 2: combos[HANDKIND_TWO]++; break;
+            case 3: combos[HANDKIND_THREE]++; break;
+            case 4: combos[HANDKIND_FOUR]++; break;
+            case 5: combos[HANDKIND_FIVE]++; break;
+            default: break;
+        }
+    }
+    if (combos[HANDKIND_FIVE])
+        return HANDKIND_FIVE;
+    if (combos[HANDKIND_FOUR])
+        return HANDKIND_FOUR;
+    if (combos[HANDKIND_THREE] && combos[HANDKIND_TWO])
+        return HANDKIND_FULL;
+    if (combos[HANDKIND_THREE])
+        return HANDKIND_THREE;
+    if (combos[HANDKIND_TWO] == 2)
+        return HANDKIND_TWO;
+    if (combos[HANDKIND_TWO])
+        return HANDKIND_ONE;
+    return HANDKIND_HI;
+}
+
+struct hand parse_hand_2(char **s)
+{
+    struct hand h;
+
+    assert(strlen(s[0]) == 5);
+
+    for (int i = 0; i < 5; i++)
+        h.cards[i] = s[0][i];
+
+    for (int i = 0; i < 5; i++)
+        h.valus[i] = card_2_hex_2(h.cards[i]);
+
+    h.bet = atoi(s[1]);
+    h.kind = hand_kind_2(&h);
+
+    return h;
+}
+
 void p2()
 {
-    int ans = 0;
+    i64 ans = 0;
+    char buf[1024];
+    struct hand *hands = NULL;
 
-    printf("p2: %d\n", ans);
+    // For this, we translate the cards to hex digits, sort the hands such that they are "reverse"
+    // sorted, then score each hand for the betting parts.
+
+    while (buf == bfgets(buf, sizeof buf, stdin)) {
+        char **strs = find_all_strs(buf);
+
+        struct hand curr = parse_hand_2(strs);
+        arrput(hands, curr);
+
+        arrfree(strs);
+    }
+
+    qsort(hands, arrlen(hands), sizeof(hands[0]), hand_cmp);
+
+    for (int i = 0; i < arrlen(hands); i++) {
+        ans += hands[i].bet * (arrlen(hands) - i);
+#if 1
+        fprintf(stderr, "\t%.*s - %.*s - %d - %d - %ld\n",
+            5, hands[i].cards, 5, hands[i].valus, hands[i].kind, hands[i].bet, ans
+        );
+#endif
+    }
+
+    printf("p2: %ld\n", ans);
+
+    arrfree(hands);
 }
 
 int main(int argc, char **argv)
